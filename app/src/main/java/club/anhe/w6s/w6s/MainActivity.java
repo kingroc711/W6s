15ada0791f17742f117e841b3f1a722af322b620
kingroc711 <960889.qq.com>
2016-03-04 13:09:25 +0800

    Initial commit

---
+++ app/src/main/java/club/anhe/w6s/w6s/MainActivity.java
@@ -0,0 +1,13 @@
+package club.anhe.w6s.w6s;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
